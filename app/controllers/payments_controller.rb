99b2744fc9d05b9d73c3faffacd5ac8e8644771c
jeremira <jmiraille@gmail.com>
2017-12-15 18:28:11 +0900

    add stripe and gem, waiting for test...

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,6 +5,11 @@ class PaymentsController < ApplicationController
   end
 
   def create
+    amount = 3000
+    payment = current_user.payments.build
+    payment.process_stripe_charge(amount, params[:stripeEmail], params[:stripeToken])
+    #handle flash message depending of payment:message
+    redirect_to current_user
   end
-  
+
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -3,5 +3,37 @@ class Payment < ApplicationRecord
 
   validates :via,    presence: true
   validates :amount, numericality: {greater_than: 0}
-  
+
+  def process_stripe_charge(amount, email, token)
+    begin
+      customer = Stripe::Customer.create(
+        :email => email,
+        :source  => token
+      )
+      charge = Stripe::Charge.create(
+        :customer    => customer.id,
+        :amount      => amount,
+        :description => 'French lesson',
+        :currency    => 'usd'
+      )
+    rescue Stripe::CardError => e
+      amount_payed = 0
+      error =  e.message
+    rescue
+      raise 'Error Stripe'
+    else
+      error = 'NONE'
+      amount_payed = amount
+    end
+    self.attributes = { via: 'stripe',
+                        amount: amount_payed,
+                        error: error }
+    if self.save
+      self
+    else
+      false
+    end
+    
+   end
+
 end
